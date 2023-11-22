01d617ba4fde40950dd10250ea9bcd33c0bc777d
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-11-23 00:27:55 +0100

    Solution for 7.2

--- src/main/java/pl/javahowtoprogramgui/section_7/e7_2/DrawSquareSpiralController.java
+++ src/main/java/pl/javahowtoprogramgui/section_7/e7_2/DrawSquareSpiralController.java
@@ -18,30 +18,30 @@ public class DrawSquareSpiralController {
     void drawSquareSpiralButtonPressed(ActionEvent event) {
         GraphicsContext gc = canvas.getGraphicsContext2D();
 
-        double length = canvas.getHeight()/5;
+        double initialLength = canvas.getHeight() / 20;
+        double length = initialLength;
 
-        int maxSize=25;
+        int maxSize = 40;
 
-        double startX= canvas.getWidth() / 2;
-        double startY= canvas.getHeight() / 2;
+        double startX = canvas.getWidth() / 2;
+        double startY = canvas.getHeight() / 2;
         double endX;
         double endY;
-        for (int counter = 1; counter <maxSize; counter++) {
-            if(counter%2==0){
-                endX =startX+ Math.pow(-1,counter) * counter/2*length;
-            }else {
-                endX=startX;
+        for (int counter = 1; counter < maxSize; counter++) {
+
+            if (counter % 2 == 0) {
+                endX = startX + Math.pow(-1, counter/2) * length;
+                endY = startY;
+            } else {
+                endX = startX;
+                endY = startY - Math.pow(-1, counter/2 + 1) * length;
             }
 
-            if((counter+1)%2==0){
-                endY=startY;
-            }else {
-                endY =startY+ Math.pow(-1,counter) * counter /2*length;
-            }
-            gc.strokeLine(startX,startY,endX,endY);
-            length=(double) (counter/2)*length;
-            startX=endX;
-            startY=endY;
+            gc.strokeLine(startX, startY, endX, endY);
+
+            length = length + ((counter+1) % 2) * initialLength;
+            startX = endX;
+            startY = endY;
         }
     }
 }
