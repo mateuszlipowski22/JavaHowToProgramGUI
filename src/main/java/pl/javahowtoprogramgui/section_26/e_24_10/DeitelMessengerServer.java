4b58bf72dab93a3d5c65a9e1ea575a6cb2af3375
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2025-03-26 23:45:03 +0100

    e_24_10 DeitelMessenger

---
+++ src/main/java/pl/javahowtoprogramgui/section_26/e_24_10/DeitelMessengerServer.java
@@ -0,0 +1,39 @@
+package pl.javahowtoprogramgui.section_26.e_24_10;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static pl.javahowtoprogramgui.section_26.e_24_10.SocketMessengerConstants.*
+
+public class DeitelMessengerServer implements MessageListener{
+
+    private ExecutorService serverExecutor;
+
+    public void startServer(){
+        serverExecutor = Executors.newCachedThreadPool();
+
+        try{
+            ServerSocket serverSocket = new ServerSocket(SERVER_PORT, 100);
+            System.out.printf("%s%d%s","Server listening on port ", SERVER_PORT);
+
+            while(true){
+                Socket clientSocket = serverSocket.accept();
+                serverExecutor.execute(new MessageReceiver(this, clientSocket));
+                System.out.println("Connection received from: "+clientSocket.getInetAddress());
+
+            }
+
+        }catch (IOException ioException){
+            ioException.printStackTrace();
+        }
+    }
+
+    @Override
+    public void messageReceived(String from, String message) {
+        String completeMessage = from + MESSAGE_SEPARATOR + message;
+        serverExecutor.execute(new MulticastSender(completeMessage.getBytes()));
+    }
+}
---
+++ src/main/java/pl/javahowtoprogramgui/section_26/e_24_10/MessageListener.java
@@ -0,0 +1,5 @@
+package pl.javahowtoprogramgui.section_26.e_24_10;
+
+public interface MessageListener {
+    public void messageReceived(String from, String message);
+}
